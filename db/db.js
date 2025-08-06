b02c18a59905e9069680c51e944efa43b40186da
unknown <rishishrivastav28@gmail.com>
2025-08-06 23:08:48 +0530

    fix: updated DB connection to use MONGODB_URI from .env

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const mongoose= require('mongoose');
 async function connectDB() {
     try {
         
-        await mongoose.connect("mongodb://127.0.0.1:27017/notesweb_proj")
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log(">>>>>>>>>>>>>>>>>>>>>>>Connected to the database successfully>>>>>>>>>>>>>>>>>>>>>>>");
     } catch (error) {
         console.error("Error connecting to the database:", error);
