f16f207a1876fe628653042b972718e3b2f6012b
unknown <rishishrivastav28@gmail.com>
2025-08-07 07:43:21 +0530

    cors updated

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ require("dotenv").config();
 
 const app = express();
 
-
 app.use(cors({
-  origin: process.env.CLIENT_URL,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  credentials: true
+  origin: ["http://localhost:3000", "https://notes-web-fe.vercel.app"],
+  credentials: true,
 }));
 
 
